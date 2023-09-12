50ee74fafa2227282ba361b2029f21269f04e2b3
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2023-09-11 21:45:30 -0500

    stuff

--- src/main.tsx
+++ src/main.tsx
@@ -12,17 +12,22 @@ import NavBar from "./tsx/components/NavBar";
 import BottomBar from "./tsx/components/Bottombar";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  // <React.StrictMode>
-  <BrowserRouter>
-    <NavBar />
-    <Routes>
-      <Route path="/" element={<Home />} errorElement={<Error />} />
-      <Route path="/adopta" element={<Adopt />} errorElement={<Error />} />
-      <Route path="/centros" element={<Centers />} errorElement={<Error />} />
-      <Route path="/adoptamos" element={<WeAdopt />} errorElement={<Error />} />
-      <Route path="*" element={<Error />} />
-    </Routes>
-    <BottomBar />
-  </BrowserRouter>,
-  //</React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} errorElement={<Error />} />
+        <Route path="/adopta" element={<Adopt />} errorElement={<Error />} />
+        <Route path="/centros" element={<Centers />} errorElement={<Error />} />
+        <Route
+          path="/adoptamos"
+          element={<WeAdopt />}
+          errorElement={<Error />}
+        />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <BottomBar />
+    </BrowserRouter>
+    ,
+  </React.StrictMode>,
 );
